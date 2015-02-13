31f734243c412a4e864652ba8a9e9ded033dc2f5
zellthatsme <slainwarrior@hotmail.com>
2015-02-13 17:52:03 +0000

    webgl ftw

--- gui/js/main.js
+++ gui/js/main.js
@@ -1,119 +1,69 @@
 $(document).ready(function(){
 	init();	
-})
-
-Array.prototype.clone = function() {
-	return this.slice(0);
-};
+});
 
 var self = this;
-var canvas, context;
 var block_size = 8;
 var block_count = 1;
 var colours = {};
-var t = 300;
-var render_int;
+var interval = 300;
 var client;
 
+var stage;
+var graphics;
+var inited = false;
+
 function init(){
+	three();
 	grab_colours();
 	client = new Faye.Client('http://172.30.152.64:8080/faye');
-    canvas = new fabric.Canvas('canvas', {
-    	renderOnAddRemove: false,
-    	stateful: false
-    });
-	context = canvas.getContext('2d');
     client.subscribe('/config', function(data){
-    	console.log(data);
-    	block_count = data['grid_size'];
-    	t = data['interval'] * 1000;
-    	init_rects();
-	    client.subscribe('/grid', function(data){
-			draw_data(data);
-	    });
+    	if(!inited){    		
+			block_count = data['grid_size'];
+			interval = data['interval'] * 1000;
+			inited = true;
+    	}
+    });
+    client.subscribe('/grid', function(data){
+		draw_data(data);
     });
 }
 
 function three(){
-	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-	
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+	stage = new PIXI.Stage(0x333333);
+	var renderer = PIXI.autoDetectRenderer(800, 800);
 
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+	$('#wrapper').append(renderer.view);
 
-	camera.position.z = 5;
-	function render() {
-		requestAnimationFrame( render );
-		renderer.render( scene, camera );
+	requestAnimFrame(animate);
+	graphics = new PIXI.Graphics();
+	stage.addChild(graphics);
+
+	function animate(){
+	    requestAnimFrame(animate);
+	    renderer.render(stage);
 	}
-	render();
 }
 
-function init_rects(){
-	canvas.clear();
-	var c = block_count - 1;
-	for(c; c != 0; c--){
-		var r = block_count - 1;
-		for(r; r != 0; r--){
-			var rect = new fabric.Rect({
-				left: c * block_size,
-				top: r * block_size,
-				fill: 'red',
-				width: block_size,
-				height: block_size,
-				selectable: false,
-				opacity: 0
-			})
-			canvas.add(rect);
-		}
-	}
-	canvas.renderAll();
+function draw_block(x, y, color){
+	graphics.beginFill(color, 1);
+	// graphics.lineStyle(5, 0xFF0000);
+	graphics.drawRect(x, y, 8, 8);
 }
 
 function draw_data(data){
-	var i = 0;
+	graphics.clear();
 	var c = block_count - 1;
+	var colour = colours['block'];
 	for(c; c != 0; c--){
 		var r = block_count - 1;		
 		for(r; r != 0; r--){
-			var state = data[c][r];
-			var rect = canvas.item(i);
-			if(state){
-				rect.set({
-					fill: colours['block'].toHexString(),
-					opacity: 1
-				})
-				// rect.animate('opacity', 1, {
-				// 	duration: t / 5,
-			 //  		// easing: fabric.util.ease.easeInCubic
-				// }); 
+			if(data[c][r]){
+				draw_block(c*8, r*8, "0x" + colour.toHex());
+				colour = tinycolor(colour).spin(1);
 			}
-			else{
-				rect.set({
-					opacity: 0
-				})
-				// rect.animate('opacity', 0, {
-				// 	duration: t * 0.8
-				// }); 
-			}
-			i++;
 		}
-		colours['block'] = tinycolor(colours['block']).spin(10);
 	}
-	// clearInterval(render_int);
-	// render_int = setInterval(function(){
-	// 	canvas.renderAll()
-	// }, t / 10);
-	canvas.renderAll();
 }
 
 function gen_data(){
